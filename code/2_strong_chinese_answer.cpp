32a1c9ea94a0f0e5becb89f100ea6841e287721d
Nikita Gaevoy <nikgaevoy@gmail.com>
2022-09-20 03:58:37 +0300

    Fix the root bug

--- code/2_strong_chinese_answer.cpp
+++ code/2_strong_chinese_answer.cpp
@@ -269,8 +269,9 @@ vector<int> solve(size_t n, const vector<tuple<int, int, int>> &edges, int root 
 	}
 
 	minheap<int> pq(gr[root].begin(), gr[root].end());
-	used[root] = true;
 	vector<bool> used(n);
+	used[root] = true;
+	
 	vector<int> ans;
 
 	while (!pq.empty())
